d6513b867570712f728638c23edad1fe7c11132f
Anna Smith <98132108+nakroMS@users.noreply.github.com>
2023-04-18 20:30:38 +0000

    Level 1 SQL Practice

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,7 +1,9 @@
 -- Create a table in the database to store customer
 -- responses to our anniversary invitation.
-
+CREATE TABLE AnivAttendees (
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
-
+CustomerID integer,
+PartySize integer
+);
 -- Hint: SQLite offers the INTEGER and REAL datatypes
--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,17 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT Type, Name, Price, Description
+FROM Dishes
+ORDER BY Price;
+
 -- Create a report showing appetizers and beverages.
+SELECT Type, Name, Price, Description 
+FROM Dishes
+WHERE Type = 'Appetizer' OR type = 'Beverage'
+ORDER BY Type;
 -- Create a report with all items except beverages.
+SELECT Type, Name, Price, Description
+FROM Dishes 
+WHERE Type != 'Beverage'
+ORDER BY Type; 
