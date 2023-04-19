5698067c00cda1adbd032efc32c7f1d07234b2f5
Anna Smith <98132108+nakroMS@users.noreply.github.com>
2023-04-19 18:37:58 +0000

    Library Problems Part 1

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,26 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+
+SELECT COUNT(*)
+FROM BOOKS b
+WHERE b.title = 'Dracula'
+
+
+SELECT COUNT(*)
+FROM BOOKS b
+LEFT JOIN Loans l
+ON b.BookID = l.BookID
+WHERE b.title = 'Dracula'
+AND l.ReturnedDate IS NULL
+
+SELECT 
+(SELECT COUNT(b.title)
+FROM BOOKS b
+WHERE b.title = 'Dracula')-
+(SELECT COUNT(b.title)
+FROM BOOKS b
+LEFT JOIN Loans l
+ON b.BookID = l.BookID
+WHERE b.title = 'Dracula'
+AND l.ReturnedDate IS NULL) as AvailableCount;
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,14 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES ((SELECT BookID FROM BOOKS WHERE Barcode = 2855934983), (SELECT PatronID FROM Patrons WHERE email = 'jvaan@wisdompets.com'),
+'2022-08-25', '2022-09-08'),
+((SELECT BookID FROM BOOKS WHERE Barcode = 4043822646), (SELECT PatronID FROM Patrons WHERE email = 'jvaan@wisdompets.com'),
+'2022-08-25', '2022-09-08');
+
+SELECT * 
+FROM Loans
+ORDER BY LoanID DESC
+LIMIT 5
